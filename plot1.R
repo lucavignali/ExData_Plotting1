6fa748f1b45a2b90a3fa271585d78d4b41f93f6b
Luca Vignali <luca_vignali@libero.it>
2014-12-05 17:05:55 +0100

    Simplified Plot1.R

--- plot1.R
+++ plot1.R
@@ -4,26 +4,18 @@
 
 ## Load the full file "household_power_consumption.txt" in PC data frame.
 ## Coerce all fields to character to manage the "?"
-allclasses <- vector("character",9)
-allclasses[1:9] <- "character"
 PC <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, 
-                 colClasses = allclasses)
+                 colClasses = "character")
 
 
-## Convert Date and Time columns  according to instructions
+## Convert Date according to instructions
 PC_Date <- as.Date(PC[,"Date"], "%d/%m/%Y") 
-PC_Time <- format(strptime(PC[,"Time"], "%H:%M:%S"), "%H:%M:%S")
-
-
-## Replace Date and Time columns in PC with PC_Date and PC_Time
-PC[,"Date"] <- PC_Date
-PC[,"Time"] <- PC_Time
 
 
 ## Extract the Variable for plot 1. Global Active Power in 1-2 February 2007.
 ## Remove question marks and coerce to numeric.
-iFeb2007 <- (PC[,"Date"] >= as.Date("2007-02-01") & 
-                     PC[,"Date"] <= as.Date("2007-02-02") & 
+iFeb2007 <- (PC_Date >= as.Date("2007-02-01") & 
+                     PC_Date <= as.Date("2007-02-02") & 
                      PC[,"Global_active_power"] != "?")
 GAP_Plot1 <- as.numeric(PC[iFeb2007,"Global_active_power"]) 
 
